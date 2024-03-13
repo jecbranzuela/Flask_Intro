79b6cb80e2d8915d7f9b4df88bc35163275c9d8f
jecbranzuela <jb3435john@gmail.com>
2024-03-13 23:39:52 +0800

    hello

--- run.py
+++ run.py
@@ -7,3 +7,5 @@ if __name__ == "__main__":
     with app.app_context(): #create the database, if not already created
         db.create_all()
     app.run(host="0.0.0.0", port=8080, threaded=True, debug=True)
+
+    #hello
